fa14cb2ff5735610816729f0193425be66f6e297
Minh Tú Lê <leminhtu74@gmail.com>
2022-10-10 14:47:44 +0700

    crawl description tag

--- VNECrawler.py
+++ VNECrawler.py
@@ -6,12 +6,14 @@ from utils import read_file, extract_content
 def main(urls_fpath="urls.txt", output_dpath="data"):
     urls = read_file(urls_fpath)
     for i, url in enumerate(urls):
-        title, paragraphs = extract_content(url)
+        title, description, paragraphs = extract_content(url)
         output_fpath = f"{output_dpath}/url_{i}.txt"
         with open(output_fpath, "w", encoding="utf-8") as file:
-            file.write(title)
+            file.write(title + "\n")
+            for p in description:
+                file.write(p + "\n")
             for p in paragraphs:
-                file.write(p)
+                file.write(p + "\n")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -11,7 +11,9 @@ def extract_content(url):
     content = requests.get(url).content
     soup = BeautifulSoup(content, "html.parser")
 
-    title = soup.find("h1", class_="title-detail").text
-    paragraphs = (p.text + "\n" for p in soup.find_all("p", class_="Normal"))
-    return title, paragraphs
+    title = soup.find("h1", class_="title-detail").text 
+    # some sport news have location-stamp child tag inside description tag
+    description = (p.text for p in soup.find("p", class_="description").contents)
+    paragraphs = (p.text for p in soup.find_all("p", class_="Normal"))
+    return title, description, paragraphs
     
